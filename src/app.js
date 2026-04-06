0baf37939cc00abb8a0aa88bb4eb3a991a4646e0
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2026-04-06 11:19:13 +0545

    commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import express from "express";
 const app = express();
 
-
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello from acquisitions!" });
+  res.status(200).json({ message: "Hello from acquisitions!" });
 });
 
 export default app;
