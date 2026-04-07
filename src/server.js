19c8d2b10ad6449d6fc84a2b3ffcdfbaf53eb5d0
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2026-04-07 12:38:46 +0545

    feat: implement graceful shutdown for server on SIGINT and SIGTERM

--- src/server.js
+++ src/server.js
@@ -5,3 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
+async function shutdown() {
+  console.log("Shutting down server...");
+  process.exit(0);
+}
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
